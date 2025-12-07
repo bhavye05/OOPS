114d6fa17829323ee72d20e0eee636e68d10ea31
bhavye05 <bhavye05@gmail.com>
2025-12-07 23:56:54 +0530

    Add script to generate first n prime numbers
    This script generates the first n prime numbers based on user input.

---
+++ Genrate first n prime nos .py
@@ -0,0 +1,12 @@
+n = int(input("Enter count: "))
+count = 0
+num = 2
+
+while count < n:
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
+        count += 1
+    num += 1
